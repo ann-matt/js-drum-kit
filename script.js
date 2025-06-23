8075fb27eda1dcfcc0d96e7f17af74c44ac335f1
Ann Matt <anmatyushkova@gmail.com>
2025-06-23 05:26:45 +0300

    mob clicks

--- script.js
+++ script.js
@@ -15,4 +15,16 @@ function removeTransition(e) {
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+
+     key.addEventListener('click', function () {
+        const keyCode = this.getAttribute('data-key');
+        const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+        
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        this.classList.add('playing');
+});
+});
