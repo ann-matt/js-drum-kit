933cafeffdeaab4dc27d4c964934d48ef38027af
Ann Matt <anmatyushkova@gmail.com>
2025-06-23 05:43:27 +0300

    fivicon error fix

--- script.js
+++ script.js
@@ -34,14 +34,4 @@ function removeTransition(e) {
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
     key.addEventListener('transitionend', removeTransition);
-
-     /* key.addEventListener('click', function () {
-        const keyCode = this.getAttribute('data-key');
-        const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-        
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-        this.classList.add('playing');
-});*/
 });
